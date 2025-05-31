fdfc6b2282fb5989101de1803b111b3578f01798
Caedis <Caedis@users.noreply.github.com>
2025-05-31 05:46:17 -0500

    GTNHLib configs + Block/Item enums

---
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/ModItems.java
@@ -0,0 +1,65 @@
+package com.fouristhenumber.utilitiesinexcess;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import com.fouristhenumber.utilitiesinexcess.common.items.ItemHeavenlyRing;
+import com.fouristhenumber.utilitiesinexcess.common.items.ItemHungerAxe;
+import com.fouristhenumber.utilitiesinexcess.common.items.ItemMobJar;
+import com.fouristhenumber.utilitiesinexcess.config.ItemConfig;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+// Credit to Et Futurum (Requiem)
+public enum ModItems {
+    // spotless:off
+
+    // make sure to leave a trailing comma
+    HUNGER_AXE(ItemConfig.enableHungerAxe, new ItemHungerAxe(), "hungerAxe"),
+    HEAVENLY_RING(ItemConfig.enableHeavenlyRing, new ItemHeavenlyRing(), "heavenlyRing"),
+    MOB_JAR(ItemConfig.enableMobJar, new ItemMobJar(), "mobJar"),
+
+
+    ; // leave trailing semicolon
+    // spotless:on
+
+    public static final ModItems[] VALUES = values();
+
+    public static void init() {
+        for (ModItems item : VALUES) {
+            if (item.isEnabled()) {
+                GameRegistry.registerItem(item.get(), item.name);
+            }
+        }
+    }
+
+    final private boolean isEnabled;
+    final private Item theItem;
+    private final String name;
+
+    ModItems(boolean enabled, Item item, String name) {
+        isEnabled = enabled;
+        theItem = item;
+        this.name = name;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public Item get() {
+        return theItem;
+    }
+
+    public ItemStack newItemStack() {
+        return newItemStack(1);
+    }
+
+    public ItemStack newItemStack(int count) {
+        return newItemStack(count, 0);
+    }
+
+    public ItemStack newItemStack(int count, int meta) {
+        return new ItemStack(this.get(), count, meta);
+    }
+}
---
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/config/ItemConfig.java
@@ -0,0 +1,17 @@
+package com.fouristhenumber.utilitiesinexcess.config;
+
+import com.fouristhenumber.utilitiesinexcess.UtilitiesInExcess;
+import com.gtnewhorizon.gtnhlib.config.Config;
+
+@Config(modid = UtilitiesInExcess.MODID, category = "items")
+public class ItemConfig {
+
+    @Config.DefaultBoolean(true)
+    public static boolean enableHungerAxe;
+
+    @Config.DefaultBoolean(true)
+    public static boolean enableHeavenlyRing;
+
+    @Config.DefaultBoolean(true)
+    public static boolean enableMobJar;
+}
