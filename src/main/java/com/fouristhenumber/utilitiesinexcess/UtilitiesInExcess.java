210be8a82b45e4e12b08877946e35de311181e92
FourIsTheNumber <33456283+FourIsTheNumber@users.noreply.github.com>
2025-05-30 23:42:52 -0400

    Add mob jar (#3)
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

--- src/main/java/com/fouristhenumber/utilitiesinexcess/UtilitiesInExcess.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/UtilitiesInExcess.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import com.fouristhenumber.utilitiesinexcess.common.blocks.BlockFloating;
 import com.fouristhenumber.utilitiesinexcess.common.items.ItemHeavenlyRing;
 import com.fouristhenumber.utilitiesinexcess.common.items.ItemHungerAxe;
+import com.fouristhenumber.utilitiesinexcess.common.items.ItemMobJar;
 import com.fouristhenumber.utilitiesinexcess.utils.EventHandler;
 import com.myname.mymodid.Tags;
 
@@ -39,6 +40,7 @@ public class UtilitiesInExcess {
     public void preInit(FMLPreInitializationEvent event) {
         GameRegistry.registerBlock(new BlockFloating(), BlockFloating.ItemBlockFloating.class, "floatingBlock");
         GameRegistry.registerItem(new ItemHungerAxe(), "hungerAxe");
+        GameRegistry.registerItem(new ItemMobJar(), "mobJar");
         GameRegistry.registerItem(new ItemHeavenlyRing(), "heavenlyRing");
 
         proxy.preInit(event);
