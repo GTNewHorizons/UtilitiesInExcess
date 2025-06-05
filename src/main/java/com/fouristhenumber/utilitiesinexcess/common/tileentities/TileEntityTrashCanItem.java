8d31c1196a1acaf556ccbbbb756a792dc012bb56
jurrejelle <jurre@jilles.com>
2025-06-05 09:26:14 +0200

    Add sided insertion voiding

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntityTrashCanItem.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntityTrashCanItem.java
@@ -1,6 +1,9 @@
 package com.fouristhenumber.utilitiesinexcess.common.tileentities;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
@@ -19,7 +22,7 @@ import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.cleanroommc.modularui.widgets.slot.SlotGroup;
 import com.fouristhenumber.utilitiesinexcess.utils.VoidingInventory;
 
-public class TileEntityTrashCanItem extends TileEntity implements IGuiHolder<PosGuiData> {
+public class TileEntityTrashCanItem extends TileEntity implements IGuiHolder<PosGuiData>, ISidedInventory {
 
     @Override
     public ModularPanel buildUI(PosGuiData data, PanelSyncManager syncManager, UISettings settings) {
@@ -63,5 +66,79 @@ public class TileEntityTrashCanItem extends TileEntity implements IGuiHolder<Pos
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return 999;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slotIn) {
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int index) {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+    }
+
+    @Override
+    public String getInventoryName() {
+        return "Trash Can (Item)";
+    }
+
+    @Override
+    public boolean hasCustomInventoryName() {
+        return true;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 999;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return false;
+    }
+
+    @Override
+    public void openInventory() {
+
+    }
+
+    @Override
+    public void closeInventory() {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int p_94128_1_) {
+        return new int[]{1,2,3,4,5,6,7,8,9,10};
+    }
+
+    @Override
+    public boolean canInsertItem(int p_102007_1_, ItemStack p_102007_2_, int p_102007_3_) {
+        return true;
+    }
+
+    @Override
+    public boolean canExtractItem(int p_102008_1_, ItemStack p_102008_2_, int p_102008_3_) {
+        return false;
     }
 }
