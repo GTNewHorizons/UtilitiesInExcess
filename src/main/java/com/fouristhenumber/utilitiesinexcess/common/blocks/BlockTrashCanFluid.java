a1cffa41014658121be2028d3a750bd192444cab
jurrejelle <jurre@jilles.com>
2025-06-05 15:58:57 +0200

    Add fluid voiding on right click

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockTrashCanFluid.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockTrashCanFluid.java
@@ -11,6 +11,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidContainerRegistry;
 
 import com.cleanroommc.modularui.factory.GuiFactories;
 import com.fouristhenumber.utilitiesinexcess.common.tileentities.TileEntityTrashCanFluid;
@@ -24,11 +25,27 @@ public class BlockTrashCanFluid extends BlockContainer {
     }
 
     @Override
-    public boolean onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer playerIn, int side, float hitX,
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX,
         float hitY, float hitZ) {
-        if (!worldIn.isRemote) {
+        if (world.isRemote) {
+            return true;
+        }
+
+        ItemStack heldItem = player.getCurrentEquippedItem();
+        if (heldItem == null) {
             GuiFactories.tileEntity()
-                .open(playerIn, x, y, z);
+                .open(player, x, y, z);
+            return true;
+        }
+
+        if (FluidContainerRegistry.isFilledContainer(heldItem)) {
+            ItemStack emptyContainer = FluidContainerRegistry.drainFluidContainer(heldItem);
+            emptyContainer.stackSize = heldItem.stackSize;
+            player.inventory.setInventorySlotContents(player.inventory.currentItem, emptyContainer);
+            return true;
+        } else {
+            GuiFactories.tileEntity()
+                .open(player, x, y, z);
         }
         return true;
     }
