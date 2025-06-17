454b53c24da68d819758de7cb8dcbf78b3e6d712
dibbydoda <93301589+dibbydoda@users.noreply.github.com>
2025-06-17 20:43:50 +1000

    gtnhlib min version requirement

--- src/main/java/com/fouristhenumber/utilitiesinexcess/UtilitiesInExcess.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/UtilitiesInExcess.java
@@ -29,7 +29,7 @@ import cpw.mods.fml.relauncher.Side;
     version = Tags.VERSION,
     name = "UtilitiesInExcess",
     acceptedMinecraftVersions = "[1.7.10]",
-    dependencies = "required-after:gtnhlib@[0.5.14,)")
+    dependencies = "required-after:gtnhlib@[0.6.31,)")
 public class UtilitiesInExcess {
 
     public static final String MODID = "utilitiesinexcess";
