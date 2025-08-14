9da305eb51da46f8c9e594116614ebb9d30e55dc
Mary Hopson <doyoumined@gmail.com>
2025-08-14 07:25:46 -0400

    Redstone generator

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/generators/TileEntityRedstoneGenerator.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/generators/TileEntityRedstoneGenerator.java
@@ -1,72 +1,150 @@
 package com.fouristhenumber.utilitiesinexcess.common.tileentities.generators;
 
-import java.util.List;
-
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemPotion;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.nbt.NBTTagCompound;
 
-public class TileEntityRedstoneGenerator extends TileEntityBaseGeneratorWithItemFuel {
+import com.cleanroommc.modularui.factory.PosGuiData;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.utils.item.IItemHandler;
+import com.cleanroommc.modularui.utils.item.InvWrapper;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.layout.Grid;
+import com.cleanroommc.modularui.widgets.slot.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import com.cleanroommc.modularui.widgets.slot.SlotGroup;
 
-    public TileEntityRedstoneGenerator() {
-        super(5000000, 5000);
-    }
+public class TileEntityRedstoneGenerator extends TileEntityLavaGenerator implements IInventory {
 
-    @Override
-    protected int getRFPerTick(ItemStack currentBurningItem) {
-        return 1000;
-    }
-
-    @Override
-    protected int getFuelBurnTime(ItemStack stack) {
-        if (stack == null) return 0;
-        return getPotionComplexity(stack) * 600;
-    }
-
-    // Each effect is 1 complexity. This is not relevant in vanilla but will make it compatible with custom modded
-    // potions.
-    // Similarly, each level above 1 gives an additional point due to requiring glowstone. Again, modded potions could
-    // be even stronger.
-    // If the duration is longer than 3600 ticks, 1 point because redstone has been applied.
-    // If the potion is splash, 1 point because gunpowder has been applied.
-    protected int getPotionComplexity(ItemStack potion) {
-        if (!(potion.getItem() instanceof ItemPotion)) return 0;
-        List<PotionEffect> effects = Items.potionitem.getEffects(potion);
-        // Awkward and mundane potions are still 1 step!
-        if (effects == null || effects.isEmpty()) return 1;
-
-        int complexity = 1;
-
-        for (PotionEffect e : effects) {
-            complexity += 1;
-            complexity += e.getAmplifier();
-
-            if (e.getDuration() > 3600) complexity += 1;
-
-            // These effects grant an additional point because they are brewed using the additional fermented spider eye
-            // step.
-            if (e.getPotionID() == Potion.harm.id || e.getPotionID() == Potion.invisibility.id) {
-                complexity += 1;
-            }
-        }
-
-        if (ItemPotion.isSplash(potion.getItemDamage())) {
-            complexity += 1;
-        }
-
-        return complexity;
-    }
-
-    @Override
-    public String getInventoryName() {
-        return "tile.potion_generator.name";
-    }
+    protected ItemStack redstoneStack = null;
 
     @Override
     public boolean isItemValidForSlot(int slot, ItemStack stack) {
         if (stack == null) return false;
-        return stack.getItem() instanceof ItemPotion;
+        return stack.getItem() == Items.redstone;
+    }
+
+    @Override
+    protected boolean consumeFuel() {
+        if (fluid != null && energyStorage.getEnergyStored() < energyStorage.getMaxEnergyStored()
+            && fluid.amount >= 125
+            && redstoneStack != null
+            && redstoneStack.getItem() != null) {
+            currentFuelBurnTime = 312;
+            currentRFPerTick = 80;
+            fluidTank.drain(125, true);
+            redstoneStack.stackSize--;
+            if (redstoneStack.stackSize <= 0) redstoneStack = null;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected String getGUIName() {
+        return getInventoryName();
+    }
+
+    @Override
+    public String getInventoryName() {
+        return "tile.redstone_generator.name";
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot) {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack stack) {
+        redstoneStack = stack;
+    }
+
+    @Override
+    public boolean hasCustomInventoryName() {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(net.minecraft.entity.player.EntityPlayer player) {
+        return true;
+    }
+
+    @Override
+    public void openInventory() {}
+
+    @Override
+    public void closeInventory() {}
+
+    @Override
+    public int getSizeInventory() {
+        return 1;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+        return redstoneStack;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int count) {
+        if (redstoneStack != null) {
+            ItemStack stack;
+            if (redstoneStack.stackSize <= count) {
+                stack = redstoneStack;
+                redstoneStack = null;
+            } else {
+                stack = redstoneStack.splitStack(count);
+                if (redstoneStack.stackSize <= 0) {
+                    redstoneStack = null;
+                }
+            }
+            return stack;
+        }
+        return null;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        if (tag.hasKey("RedstoneStack")) {
+            redstoneStack = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("RedstoneStack"));
+        } else {
+            redstoneStack = null;
+        }
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+        if (redstoneStack != null) {
+            NBTTagCompound fuelStackTag = new NBTTagCompound();
+            redstoneStack.writeToNBT(fuelStackTag);
+            tag.setTag("RedstoneStack", fuelStackTag);
+        }
+    }
+
+    @Override
+    public ModularPanel buildUI(PosGuiData data, PanelSyncManager syncManager, UISettings settings) {
+        ModularPanel panel = super.buildUI(data, syncManager, settings);
+
+        SlotGroup slotGroup = new SlotGroup("fuel_slot", 1);
+
+        IItemHandler itemHandler = new InvWrapper(this);
+        ModularSlot slot = new ModularSlot(itemHandler, 0).slotGroup(slotGroup);
+
+        panel.child(
+            new Grid().coverChildren()
+                .pos(79, 52)
+                .mapTo(1, 1, index -> new ItemSlot().slot(slot)));
+
+        return panel;
     }
 }
