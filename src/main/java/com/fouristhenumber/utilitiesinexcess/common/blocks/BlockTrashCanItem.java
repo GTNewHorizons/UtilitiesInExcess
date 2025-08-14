a3b564317f7f9c1e2007f45f33df2718f902d098
Mary Hopson <doyoumined@gmail.com>
2025-08-13 23:52:40 -0400

    Remove pointless tooltips

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockTrashCanItem.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockTrashCanItem.java
@@ -1,15 +1,9 @@
 package com.fouristhenumber.utilitiesinexcess.common.blocks;
 
-import java.util.List;
-
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import com.cleanroommc.modularui.factory.GuiFactories;
@@ -36,17 +30,5 @@ public class BlockTrashCanItem extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileEntityTrashCanItem();
-    }
-
-    public static class ItemBlockTrashCanItem extends ItemBlock {
-
-        public ItemBlockTrashCanItem(Block block) {
-            super(block);
-        }
-
-        @Override
-        public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean bool) {
-            tooltip.add(StatCollector.translateToLocal("tile.trash_can_fluid.desc"));
-        }
     }
 }
