ab3ec25d974d1d5cbf9c17c864c2579f5e2e3777
koolkrafter5 <creepersrgreen@gmail.com>
2025-11-07 18:13:59 -0500

    Add NEI information pages to many blocks/items

--- src/main/java/com/fouristhenumber/utilitiesinexcess/client/IMCForNEI.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/client/IMCForNEI.java
@@ -12,6 +12,26 @@ public class IMCForNEI {
         sendInfoPage("<utilitiesinexcess:temporal_gate>", "nei.infopage.uie.temporal_gate.3");
         sendInfoPage("<utilitiesinexcess:temporal_gate>", "nei.infopage.uie.temporal_gate.4");
         sendInfoPage("<utilitiesinexcess:temporal_gate>", "nei.infopage.uie.temporal_gate.5");
+
+        sendInfoPage("<utilitiesinexcess:ethereal_glass>", "nei.infopage.uie.ethereal_glass.0");
+        sendInfoPage("<utilitiesinexcess:ethereal_glass:2>", "nei.infopage.uie.ethereal_glass.2");
+        sendInfoPage("<utilitiesinexcess:ethereal_glass:3>", "nei.infopage.uie.ethereal_glass.3");
+        sendInfoPage("<utilitiesinexcess:ethereal_glass:5>", "nei.infopage.uie.ethereal_glass.5");
+
+        sendInfoPage("<utilitiesinexcess:inversion_sigil_inactive>,<utilitiesinexcess:inversion_sigil_active>", "nei.infopage.uie.inversion_sigil.1");
+        sendInfoPage("<utilitiesinexcess:inversion_sigil_inactive>,<utilitiesinexcess:inversion_sigil_active>", "nei.infopage.uie.inversion_sigil.2");
+        sendInfoPage("<utilitiesinexcess:inversion_sigil_inactive>,<utilitiesinexcess:inversion_sigil_active>", "nei.infopage.uie.inversion_sigil.3");
+        sendInfoPage("<utilitiesinexcess:inversion_sigil_inactive>,<utilitiesinexcess:inversion_sigil_active>", "nei.infopage.uie.inversion_sigil.4");
+
+        sendInfoPage("<utilitiesinexcess:cursed_earth>", "nei.infopage.uie.cursed_earth.1");
+
+        sendInfoPage("utilitiesinexcess:lapis_aetherius", "nei.infopage.uie.lapis_aetherius.1");
+
+        sendInfoPage("<utilitiesinexcess:blackout_curtains>", "nei.infopage.uie.blackout_curtains.1");
+
+        sendInfoPage("<utilitiesinexcess:underworld_portal>", "nei.infopage.uie.underworld_portal.1");
+        sendInfoPage("<utilitiesinexcess:underworld_portal>", "nei.infopage.uie.underworld_portal.2");
+        sendInfoPage("<utilitiesinexcess:underworld_portal>", "nei.infopage.uie.underworld_portal.3");
     }
 
     private static void sendInfoPage(String filter, String page) {
