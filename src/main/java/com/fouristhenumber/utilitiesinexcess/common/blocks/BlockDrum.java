92444733ffc25c1a93a060b06ce646926c5190cc
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-11-07 20:03:54 -0700

    actually give the bucket back on both sides

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockDrum.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockDrum.java
@@ -51,9 +51,7 @@ public class BlockDrum extends BlockContainer {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX,
         float hitY, float hitZ) {
-        if (world.isRemote) {
-            return true;
-        }
+
         TileEntity tile = world.getTileEntity(x, y, z);
         if (tile instanceof TileEntityDrum drum) {
             ItemStack heldItem = player.getCurrentEquippedItem();
