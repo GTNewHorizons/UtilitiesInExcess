406e5bdc1a26c174a9ed49148b51abc2bf566177
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-11-23 09:00:41 +0200

    remove abs

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockConveyor.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockConveyor.java
@@ -76,7 +76,7 @@ public class BlockConveyor extends BlockContainer {
         @Override
         public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
             float hitX, float hitY, float hitZ, int metadata) {
-            int direction = Math.abs((int) ((((player.rotationYaw % 360) + 45f) / 90f + 4f) % 4f));
+            int direction = (int) ((((player.rotationYaw % 360) + 45f) / 90f + 4f) % 4f);
 
             return super.placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, direction);
         }
