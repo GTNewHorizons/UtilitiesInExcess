7c759436705c1dd0b7a4f95a2625900c12ff0c2e
Mary Hopson <doyoumined@gmail.com>
2025-12-03 07:40:41 -0500

    Fix for merge

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/renderers/InvertedIngotRenderer.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/renderers/InvertedIngotRenderer.java
@@ -1,5 +1,7 @@
 package com.fouristhenumber.utilitiesinexcess.common.renderers;
 
+import static com.fouristhenumber.utilitiesinexcess.utils.RenderUtils.renderItemIcon;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.OpenGlHelper;
@@ -116,6 +118,8 @@ public class InvertedIngotRenderer implements IItemRenderer {
             }
             case INVENTORY -> {
                 GL11.glEnable(GL11.GL_ALPHA_TEST);
+                GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
+                GL11.glEnable(GL11.GL_BLEND);
                 renderItemIcon(icon, 0.0D, 0.0D, 16.0D, 16.0D, 0.001, 0.0F, 0.0F, -1.0F);
                 GL11.glDisable(GL11.GL_ALPHA_TEST);
             }
@@ -123,29 +127,5 @@ public class InvertedIngotRenderer implements IItemRenderer {
         }
 
         GL11.glPopAttrib();
-    }
-
-    public static void renderItemIcon(IIcon icon, double xStart, double yStart, double xEnd, double yEnd, double z,
-        float nx, float ny, float nz) {
-        if (icon == null) {
-            return;
-        }
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
-        GL11.glEnable(GL11.GL_BLEND);
-        final Tessellator tess = Tessellator.instance;
-        tess.startDrawingQuads();
-        tess.setNormal(nx, ny, nz);
-        if (nz > 0.0F) {
-            tess.addVertexWithUV(xStart, yStart, z, icon.getMinU(), icon.getMinV());
-            tess.addVertexWithUV(xEnd, yStart, z, icon.getMaxU(), icon.getMinV());
-            tess.addVertexWithUV(xEnd, yEnd, z, icon.getMaxU(), icon.getMaxV());
-            tess.addVertexWithUV(xStart, yEnd, z, icon.getMinU(), icon.getMaxV());
-        } else {
-            tess.addVertexWithUV(xStart, yEnd, z, icon.getMinU(), icon.getMaxV());
-            tess.addVertexWithUV(xEnd, yEnd, z, icon.getMaxU(), icon.getMaxV());
-            tess.addVertexWithUV(xEnd, yStart, z, icon.getMaxU(), icon.getMinV());
-            tess.addVertexWithUV(xStart, yStart, z, icon.getMinU(), icon.getMinV());
-        }
-        tess.draw();
     }
 }
