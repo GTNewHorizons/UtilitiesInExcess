845c9f1e98e22199f91b7f1aa1b171bce761146e
loenaaaa <asusactiongamer@gmail.com>
2025-12-18 09:17:43 +0800

    Register ABUD's redstone state in NBT

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntityAdvancedBlockUpdateDetector.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntityAdvancedBlockUpdateDetector.java
@@ -102,6 +102,8 @@ public class TileEntityAdvancedBlockUpdateDetector extends TileEntity {
         for (int i = 0; i < ForgeDirection.VALID_DIRECTIONS.length; i++) {
             scanningOnFace[i] = (scannedFaces >> i) > 0;
         }
+        isProvidingPower = compound.getBoolean("powered");
+        pulseTimer = compound.getInteger("pulse");
     }
 
     @Override
@@ -112,6 +114,8 @@ public class TileEntityAdvancedBlockUpdateDetector extends TileEntity {
             scannedFaces += (byte) ((scanningOnFace[i] ? 1 : 0) << i);
         }
         compound.setByte("faces", scannedFaces);
+        compound.setBoolean("powered", isProvidingPower);
+        compound.setInteger("pulse", pulseTimer);
     }
 
     private void sendRedstonePulse() {
