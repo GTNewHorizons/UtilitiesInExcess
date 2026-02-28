de233e41ea95ececd58cf081f1bf868f7c1ff9bf
rjnasers <rjnasers@iastate.edu>
2026-02-28 14:11:04 +0100

    Comments

--- src/main/java/com/fouristhenumber/utilitiesinexcess/compat/Mods.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/compat/Mods.java
@@ -14,7 +14,7 @@ public enum Mods {
     CraftTweaker("MineTweaker3"),
     FindIt("findit"),
     Tinkers("TConstruct"),
-    ForgeMicroBlock("ForgeMicroBlock"),
+    ForgeMicroBlock("ForgeMicroblock"),
     Backhand("backhand"),
     GT("gregtech_nh")
     ;
