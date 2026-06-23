f873121a8f87ee598a5336b9a18fa491860642be
Spaghetti-OberNub <74196682+Spaghetti-OberNub@users.noreply.github.com>
2026-06-23 21:51:42 +0200

    Added flammability to flammable blocks (#168)

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockPacifistsBench.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockPacifistsBench.java
@@ -3,7 +3,9 @@ package com.fouristhenumber.utilitiesinexcess.common.blocks;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 import com.fouristhenumber.utilitiesinexcess.common.tileentities.TileEntityPacifistsBench;
 
@@ -23,5 +25,15 @@ public class BlockPacifistsBench extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityPacifistsBench();
+    }
+
+    @Override
+    public int getFlammability(IBlockAccess world, int x, int y, int z, ForgeDirection face) {
+        return 5;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(IBlockAccess world, int x, int y, int z, ForgeDirection face) {
+        return 10;
     }
 }
