b29f9a713fa43e08a2616a3f3608fe46434ac426
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2026-06-24 17:00:06 +0300

    waila compat

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockRainMuffler.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockRainMuffler.java
@@ -7,7 +7,9 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -19,7 +21,13 @@ import net.minecraft.world.World;
 import com.fouristhenumber.utilitiesinexcess.common.tileentities.TileEntityRainMuffler;
 import com.fouristhenumber.utilitiesinexcess.config.blocks.BlockConfig;
 
-public class BlockRainMuffler extends BlockContainer {
+import cpw.mods.fml.common.Optional;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+
+@Optional.Interface(iface = "mcp.mobius.waila.api.IWailaDataProvider", modid = "Waila")
+public class BlockRainMuffler extends BlockContainer implements IWailaDataProvider {
 
     public BlockRainMuffler() {
         super(Material.sponge);
@@ -53,17 +61,19 @@ public class BlockRainMuffler extends BlockContainer {
     @Override
     public boolean onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX,
         float subY, float subZ) {
+        if (!worldIn.isRemote) {
+            return true;
+        }
+
         NBTTagCompound playerNBT = player.getEntityData();
         NBTTagCompound persistentNBT = playerNBT.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
         boolean nowActive = !persistentNBT.getBoolean(NBT_RAIN_MUFFLED);
         persistentNBT.setBoolean(NBT_RAIN_MUFFLED, nowActive);
         playerNBT.setTag(EntityPlayer.PERSISTED_NBT_TAG, persistentNBT);
-        if (worldIn.isRemote) {
-            if (nowActive) {
-                player.addChatMessage(new ChatComponentTranslation("tile.rain_muffler.chat.global_enable"));
-            } else {
-                player.addChatMessage(new ChatComponentTranslation("tile.rain_muffler.chat.global_disable"));
-            }
+        if (nowActive) {
+            player.addChatMessage(new ChatComponentTranslation("tile.rain_muffler.chat.global_enable"));
+        } else {
+            player.addChatMessage(new ChatComponentTranslation("tile.rain_muffler.chat.global_disable"));
         }
         return true;
     }
@@ -80,5 +90,40 @@ public class BlockRainMuffler extends BlockContainer {
                 StatCollector
                     .translateToLocalFormatted("tile.rain_muffler.desc.1", BlockConfig.rainMuffler.rainMufflerRange));
         }
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
+        IWailaConfigHandler config) {
+        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        NBTTagCompound playerNBT = player.getEntityData();
+        NBTTagCompound persistentNBT = playerNBT.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+        if (persistentNBT.getBoolean(NBT_RAIN_MUFFLED)) {
+            currentTip.add(StatCollector.translateToLocal("tile.rain_muffler.waila.global_enabled"));
+        } else {
+            currentTip.add(StatCollector.translateToLocal("tile.rain_muffler.waila.global_disabled"));
+        }
+
+        return currentTip;
+    }
+
+    // Snubs
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x,
+        int y, int z) {
+        return tag;
+    }
+
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
+        IWailaConfigHandler config) {
+        return currentTip;
+    }
+
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
+        IWailaConfigHandler config) {
+        return currentTip;
     }
 }
---
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/compat/waila/WailaCompat.java
@@ -0,0 +1,32 @@
+package com.fouristhenumber.utilitiesinexcess.compat.waila;
+
+import com.fouristhenumber.utilitiesinexcess.ModBlocks;
+import com.fouristhenumber.utilitiesinexcess.common.blocks.BlockCollector;
+import com.fouristhenumber.utilitiesinexcess.common.blocks.BlockRainMuffler;
+import com.fouristhenumber.utilitiesinexcess.common.blocks.BlockSmartPump;
+import com.fouristhenumber.utilitiesinexcess.common.blocks.BlockSpike;
+import com.fouristhenumber.utilitiesinexcess.common.blocks.generators.BlockBaseGenerator;
+
+import mcp.mobius.waila.api.IWailaDataProvider;
+import mcp.mobius.waila.api.IWailaRegistrar;
+
+@SuppressWarnings("unused")
+public class WailaCompat {
+
+    // Passed to waila in CommonProxy
+    public static void callbackRegister(IWailaRegistrar registrar) {
+        registrar.registerNBTProvider((IWailaDataProvider) ModBlocks.COLLECTOR.get(), BlockCollector.class);
+        registrar.registerBodyProvider((IWailaDataProvider) ModBlocks.COLLECTOR.get(), BlockCollector.class);
+
+        registrar.registerNBTProvider((IWailaDataProvider) ModBlocks.SPIKE_WOOD.get(), BlockSpike.class);
+        registrar.registerBodyProvider((IWailaDataProvider) ModBlocks.SPIKE_WOOD.get(), BlockSpike.class);
+
+        registrar.registerNBTProvider((IWailaDataProvider) ModBlocks.SMART_PUMP.get(), BlockSmartPump.class);
+        registrar.registerBodyProvider((IWailaDataProvider) ModBlocks.SMART_PUMP.get(), BlockSmartPump.class);
+
+        registrar.registerNBTProvider(GeneratorDataProvider.INSTANCE, BlockBaseGenerator.class);
+        registrar.registerBodyProvider(GeneratorDataProvider.INSTANCE, BlockBaseGenerator.class);
+
+        registrar.registerBodyProvider((IWailaDataProvider) ModBlocks.RAIN_MUFFLER.get(), BlockRainMuffler.class);
+    }
+}
