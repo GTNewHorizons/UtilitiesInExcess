0f65d915bb5072d087e0a616e8290c3c1be8bec5
FourIsTheNumber <33456283+FourIsTheNumber@users.noreply.github.com>
2026-06-24 13:14:23 -0400

    Implement Cursed Earth spreading (#150)
    Co-authored-by: Nikolay S. <nicksitnikov@gmail.com>

--- src/main/java/com/fouristhenumber/utilitiesinexcess/config/blocks/BlockConfig.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/config/blocks/BlockConfig.java
@@ -202,7 +202,7 @@ public class BlockConfig {
         public int gigaTorchRange;
     }
 
-    @Config.Comment("Cursed Earth Configuration")
+    @Config.Comment("Spike Configuration")
     public static final Spikes spikes = new Spikes();
 
     @Config.LangKey("utilitiesinexcess.config.block.spikes")
