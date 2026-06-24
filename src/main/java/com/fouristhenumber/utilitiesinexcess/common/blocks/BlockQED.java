47c28e5037f437bb0e820447e72901593c022dc7
Mary Hopson <doyoumined@gmail.com>
2026-06-24 18:02:22 -0400

    New jextures (june textures)

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockQED.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockQED.java
@@ -2,12 +2,18 @@ package com.fouristhenumber.utilitiesinexcess.common.blocks;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.cleanroommc.modularui.factory.GuiFactories;
 import com.fouristhenumber.utilitiesinexcess.common.tileentities.TileEntityQED;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockQED extends BlockContainer {
 
@@ -40,5 +46,27 @@ public class BlockQED extends BlockContainer {
                 .open(player, x, y, z);
         }
         return true;
+    }
+
+    public IIcon sideTexture;
+    public IIcon bottomTexture;
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        this.blockIcon = reg.registerIcon(this.getTextureName() + "_top");
+        this.sideTexture = reg.registerIcon(this.getTextureName() + "_side");
+        this.bottomTexture = reg.registerIcon(this.getTextureName() + "_bottom");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta) {
+        return side == 1 ? this.blockIcon : (side == 0 ? bottomTexture : this.sideTexture);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(IBlockAccess worldIn, int x, int y, int z, int side) {
+        return getIcon(side, 0);
     }
 }
