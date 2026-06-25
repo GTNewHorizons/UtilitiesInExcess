74fb5588ba231d55871d37370401067a5cd0a219
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2026-06-25 20:35:45 +0300

    Miscellaneous minor fixes (#188)

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockSpike.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/blocks/BlockSpike.java
@@ -241,6 +241,7 @@ public class BlockSpike extends Block implements IWailaDataProvider {
         }
     }
 
+    @Optional.Method(modid = "Waila")
     @Override
     public List<String> getWailaBody(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
         IWailaConfigHandler config) {
@@ -259,6 +260,7 @@ public class BlockSpike extends Block implements IWailaDataProvider {
         return currentTip;
     }
 
+    @Optional.Method(modid = "Waila")
     @Override
     public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x,
         int y, int z) {
@@ -271,16 +273,19 @@ public class BlockSpike extends Block implements IWailaDataProvider {
         return tag;
     }
 
+    @Optional.Method(modid = "Waila")
     // Stubs
     public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
         return null;
     }
 
+    @Optional.Method(modid = "Waila")
     public List<String> getWailaHead(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
         IWailaConfigHandler config) {
         return currentTip;
     }
 
+    @Optional.Method(modid = "Waila")
     public List<String> getWailaTail(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor,
         IWailaConfigHandler config) {
         return currentTip;
