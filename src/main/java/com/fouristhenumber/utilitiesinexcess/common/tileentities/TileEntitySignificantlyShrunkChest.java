dd1049e2d1074cc072c1342a44f66a27101171d2
Mary Hopson <doyoumined@gmail.com>
2026-06-25 19:12:14 -0400

    Cleanup: Significantly Shrunk Chest

--- src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntitySignificantlyShrunkChest.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/common/tileentities/TileEntitySignificantlyShrunkChest.java
@@ -1,7 +1,5 @@
 package com.fouristhenumber.utilitiesinexcess.common.tileentities;
 
-import net.minecraft.util.StatCollector;
-
 import com.cleanroommc.modularui.api.IGuiHolder;
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.factory.PosGuiData;
@@ -11,7 +9,6 @@ import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.utils.item.IItemHandler;
 import com.cleanroommc.modularui.utils.item.InvWrapper;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
-import com.cleanroommc.modularui.widgets.layout.Grid;
 import com.cleanroommc.modularui.widgets.slot.ItemSlot;
 import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.cleanroommc.modularui.widgets.slot.SlotGroup;
@@ -30,7 +27,7 @@ public class TileEntitySignificantlyShrunkChest extends TileEntityMarginallyMaxi
 
         // Add title
         panel.child(
-            IKey.str(StatCollector.translateToLocal(getInventoryName()))
+            IKey.lang(getInventoryName())
                 .asWidget()
                 .marginLeft(5)
                 .marginTop(5));
@@ -40,9 +37,9 @@ public class TileEntitySignificantlyShrunkChest extends TileEntityMarginallyMaxi
 
         // Add item slot
         panel.child(
-            new Grid().coverChildren()
-                .pos(79, 34)
-                .mapTo(1, 1, index -> new ItemSlot().slot(slot)));
+            new ItemSlot().slot(slot)
+                .horizontalCenter()
+                .top(34));
 
         return panel;
     }
