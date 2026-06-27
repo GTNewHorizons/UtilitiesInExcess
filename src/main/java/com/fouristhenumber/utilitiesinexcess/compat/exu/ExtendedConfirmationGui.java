1a4c469392dd1ff9f57192e9948ca1f62ba4821d
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2026-06-27 16:13:46 +0300

    Add world conversion warning screen

---
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/compat/exu/ExtendedConfirmationGui.java
@@ -0,0 +1,71 @@
+package com.fouristhenumber.utilitiesinexcess.compat.exu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiLabel;
+import net.minecraft.client.gui.GuiOptionButton;
+import net.minecraft.client.resources.I18n;
+
+import com.gtnewhorizon.gtnhlib.api.gui.GuiConfirmationWCW;
+
+import cpw.mods.fml.common.StartupQuery;
+
+@SuppressWarnings("unused")
+public class ExtendedConfirmationGui extends GuiConfirmationWCW {
+
+    private final List<String> lines;
+    private final String warning;
+
+    public ExtendedConfirmationGui(StartupQuery query) {
+        super(query);
+        lines = new ArrayList<>(
+            Arrays.asList(
+                query.getText()
+                    .split("\\\\n")));
+        warning = lines.get(0);
+    }
+
+    public void initGui() {
+        this.buttonList.add(new GuiOptionButton(0, this.width / 2 - 155, this.height - 58, I18n.format("gui.yes")));
+        this.buttonList
+            .add(new GuiOptionButton(1, this.width / 2 - 155 + 160, this.height - 58, I18n.format("gui.no")));
+    }
+
+    private static byte colorTick = 0;
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+
+        colorTick++;
+
+        String color = Math.abs(colorTick % 6) >= 3 ? "§4§l" : "§c§l";
+        lines.set(0, "§kABC§r " + color + warning + " §r§kCBA");
+
+        int spaceAvailable = this.height - 58 - 20;
+        int spaceRequired = Math.min(spaceAvailable, 10 + 10 * lines.size());
+
+        int offset = 10 + (spaceAvailable - spaceRequired) / 2; // vertically centered
+        for (String line : lines) {
+            if (offset >= spaceAvailable) {
+                this.drawCenteredString(this.fontRendererObj, "...", this.width / 2, offset, 0xFFFFFF);
+                break;
+            } else {
+                if (!line.isEmpty())
+                    this.drawCenteredString(this.fontRendererObj, line, this.width / 2, offset, 0xFFFFFF);
+                offset += 10;
+            }
+        }
+
+        for (GuiButton button : this.buttonList) {
+            button.drawButton(this.mc, mouseX, mouseY);
+        }
+
+        for (GuiLabel label : this.labelList) {
+            label.func_146159_a(this.mc, mouseX, mouseY);
+        }
+    }
+}
