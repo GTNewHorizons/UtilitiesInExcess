ffb93b8e87b580bb201b9d055c300999381ccf49
Spaghetti-OberNub <74196682+Spaghetti-OberNub@users.noreply.github.com>
2026-06-28 12:23:35 +0200

    Added config block lists for reversing hoe and desctruction pickaxe  (#178)
    Co-authored-by: Mary Hopson <doyoumined@gmail.com>

--- src/main/java/com/fouristhenumber/utilitiesinexcess/config/items/unstabletools/DestructionPickaxeConfig.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/config/items/unstabletools/DestructionPickaxeConfig.java
@@ -9,18 +9,23 @@ public class DestructionPickaxeConfig {
     @Config.DefaultBoolean(true)
     @Config.RequiresMcRestart
     public static boolean enable;
+
     @Config.DefaultBoolean(true)
     @Config.RequiresMcRestart
     public static boolean unbreakable;
+
     @Config.DefaultBoolean(false)
     public static boolean voidMinedBlock;
-    @Config.DefaultStringList({ "minecraft:*stone", "minecraft:netherrack", "minecraft:*hardened_clay" })
+
+    @Config.Comment("Which blocks the pickaxe is effective against. Format as modid:blockid:meta - if meta is not specified, will use any meta.")
+    @Config.DefaultStringList({ "minecraft:stone", "minecraft:cobblestone", "minecraft:sandstone",
+        "minecraft:netherrack", "minecraft:hardened_clay", "minecraft:stained_hardened_clay" })
     public static String[] includeEffective;
-    @Config.DefaultStringList({})
-    public static String[] excludeEffective;
+
     @Config.DefaultFloat(5)
     @Config.RangeFloat(min = 0, max = 100)
     public static float effectiveSpeedModifier;
+
     @Config.DefaultFloat(0.0625f)
     @Config.RangeFloat(min = 0, max = 100)
     public static float ineffectiveSpeedModifier;
