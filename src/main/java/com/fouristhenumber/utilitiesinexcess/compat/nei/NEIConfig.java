2c63b3c0c97d3505337f009629c173a42c4fea19
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2026-07-05 19:40:52 +0300

    revert caching alias itemstacks

--- src/main/java/com/fouristhenumber/utilitiesinexcess/compat/nei/NEIConfig.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/compat/nei/NEIConfig.java
@@ -64,7 +64,6 @@ public class NEIConfig implements IConfigureNEI {
             "utilitiesinexcess@" + StatCollector.translateToLocal("nei.title.uie.pseudo_reversion")
                 + "@pseudo_reversion_recipes");
 
-        initAliasItems();
         registerAliases();
     }
 
@@ -94,59 +93,57 @@ public class NEIConfig implements IConfigureNEI {
         }
     }
 
-    // spotless:off
-    public static final ArrayList<ItemStack> ALIASED_ITEMSTACKS = new ArrayList<>(List.of(
-        new ItemStack(ModBlocks.FLOATING_BLOCK.getItem()),
-        new ItemStack(ModBlocks.UNDERWORLD_PORTAL.getItem()),
-        new ItemStack(ModBlocks.END_OF_TIME_PORTAL.getItem()),
-        new ItemStack(ModBlocks.PACIFISTS_BENCH.getItem()),
-        new ItemStack(ModBlocks.GIGA_TORCH.getItem()),
-        new ItemStack(ModBlocks.ENDSPARK.getItem()),
-        new ItemStack(ModBlocks.ENDER_LOCUS.getItem()),
-        new ItemStack(ModBlocks.INVERTED_BLOCK.getItem()),
-        new ItemStack(ModBlocks.MAGIC_WOOD.get()),
-        new ItemStack(ModBlocks.SIGNIFICANTLY_SHRUNK_CHEST.get()),
-        new ItemStack(ModBlocks.RADICALLY_REDUCED_CHEST.get()),
-        new ItemStack(ModBlocks.MARGINALLY_MAXIMISED_CHEST.get()),
-        new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_GENERATOR.get()),
-        new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_SUBSTATION.get()),
-        new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_POWERPLANT.get()),
-        new ItemStack(ModBlocks.COLLECTOR.get()),
+    public static void registerAliases() {
+        // spotless:off
+        ArrayList<ItemStack> aliasedItemStacks = new ArrayList<>(List.of(
+            new ItemStack(ModBlocks.FLOATING_BLOCK.getItem()),
+            new ItemStack(ModBlocks.UNDERWORLD_PORTAL.getItem()),
+            new ItemStack(ModBlocks.END_OF_TIME_PORTAL.getItem()),
+            new ItemStack(ModBlocks.PACIFISTS_BENCH.getItem()),
+            new ItemStack(ModBlocks.GIGA_TORCH.getItem()),
+            new ItemStack(ModBlocks.ENDSPARK.getItem()),
+            new ItemStack(ModBlocks.ENDER_LOCUS.getItem()),
+            new ItemStack(ModBlocks.INVERTED_BLOCK.getItem()),
+            new ItemStack(ModBlocks.MAGIC_WOOD.get()),
+            new ItemStack(ModBlocks.SIGNIFICANTLY_SHRUNK_CHEST.get()),
+            new ItemStack(ModBlocks.RADICALLY_REDUCED_CHEST.get()),
+            new ItemStack(ModBlocks.MARGINALLY_MAXIMISED_CHEST.get()),
+            new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_GENERATOR.get()),
+            new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_SUBSTATION.get()),
+            new ItemStack(ModBlocks.LOW_TEMPERATURE_FURNACE_POWERPLANT.get()),
+            new ItemStack(ModBlocks.COLLECTOR.get()),
 
-        new ItemStack(ModItems.ANTI_PARTICULATE_SHOVEL.get()),
-        new ItemStack(ModItems.MOB_JAR.get()),
-        new ItemStack(ModItems.INVERSION_SIGIL_INACTIVE.get()),
-        ItemInversionSigilActive.getStack(),
-        new ItemStack(ModItems.PSEUDO_REVERSION_SIGIL.get()),
-        new ItemStack(ModItems.INVERTED_INGOT.get()),
-        new ItemStack(ModItems.INVERTED_INGOT.get(), 1, 1),
-        new ItemStack(ModItems.ARCHITECTS_WAND.get()),
-        new ItemStack(ModItems.SUPER_ARCHITECTS_WAND.get()),
-        new ItemStack(ModItems.HEAVENLY_RING_FEATHER.get()),
-        new ItemStack(ModItems.HEAVENLY_RING_DRAGON.get()),
-        new ItemStack(ModItems.HEAVENLY_RING_FAIRY.get()),
-        new ItemStack(ModItems.HEAVENLY_RING_METAL.get()),
-        new ItemStack(ModItems.HEAVENLY_RING_MAGIC.get()),
-        new ItemStack(ModItems.CHUNCHUNMARU.get()),
-        new ItemStack(ModItems.FIRE_BATTERY.get()),
-        new ItemStack(ModItems.ENDER_LOTUS_SEED.get()),
-        new ItemStack(ModItems.BLOCK_ANALYZER.get()),
-        new ItemStack(ModItems.XRAY_GLASSES.get())
-    ));
-    // spotless:on
+            new ItemStack(ModItems.ANTI_PARTICULATE_SHOVEL.get()),
+            new ItemStack(ModItems.MOB_JAR.get()),
+            new ItemStack(ModItems.INVERSION_SIGIL_INACTIVE.get()),
+            ItemInversionSigilActive.getStack(),
+            new ItemStack(ModItems.PSEUDO_REVERSION_SIGIL.get()),
+            new ItemStack(ModItems.INVERTED_INGOT.get()),
+            new ItemStack(ModItems.INVERTED_INGOT.get(), 1, 1),
+            new ItemStack(ModItems.ARCHITECTS_WAND.get()),
+            new ItemStack(ModItems.SUPER_ARCHITECTS_WAND.get()),
+            new ItemStack(ModItems.HEAVENLY_RING_FEATHER.get()),
+            new ItemStack(ModItems.HEAVENLY_RING_DRAGON.get()),
+            new ItemStack(ModItems.HEAVENLY_RING_FAIRY.get()),
+            new ItemStack(ModItems.HEAVENLY_RING_METAL.get()),
+            new ItemStack(ModItems.HEAVENLY_RING_MAGIC.get()),
+            new ItemStack(ModItems.CHUNCHUNMARU.get()),
+            new ItemStack(ModItems.FIRE_BATTERY.get()),
+            new ItemStack(ModItems.ENDER_LOTUS_SEED.get()),
+            new ItemStack(ModItems.BLOCK_ANALYZER.get()),
+            new ItemStack(ModItems.XRAY_GLASSES.get())
+        ));
+        // spotless:on
 
-    public static void initAliasItems() {
         for (int i = 0; i < 16; i++) {
-            ALIASED_ITEMSTACKS.add(new ItemStack(ModBlocks.LAPIS_AETHERIUS.getItem(), 1, i));
+            aliasedItemStacks.add(new ItemStack(ModBlocks.LAPIS_AETHERIUS.getItem(), 1, i));
         }
 
         for (int i = 0; i < 10; i++) {
-            ALIASED_ITEMSTACKS.add(new ItemStack(ModBlocks.DECORATIVE_GLASS.getItem(), 1, i));
+            aliasedItemStacks.add(new ItemStack(ModBlocks.DECORATIVE_GLASS.getItem(), 1, i));
         }
-    }
 
-    public static void registerAliases() {
-        for (ItemStack itemStack : ALIASED_ITEMSTACKS) {
+        for (ItemStack itemStack : aliasedItemStacks) {
             if (itemStack.getItem() == null) continue;
 
             API.setAliases(
