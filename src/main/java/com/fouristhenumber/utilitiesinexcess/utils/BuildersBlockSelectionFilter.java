83516386a20493e8a0491ed81071ff21d3a2a16a
Cooble <minekoza@gmail.com>
2026-08-11 22:27:24 +0200

    spotlessApply

--- src/main/java/com/fouristhenumber/utilitiesinexcess/utils/BuildersBlockSelectionFilter.java
+++ src/main/java/com/fouristhenumber/utilitiesinexcess/utils/BuildersBlockSelectionFilter.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
